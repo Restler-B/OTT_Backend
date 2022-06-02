c54fcd9f4ea09d491b40641ba6b5b24de500a281
Chrisstan <60028325+Chrisstan@users.noreply.github.com>
2022-06-02 12:32:03 +0530

    Add files via upload

---
+++ Ott_DemoApp/src/main/java/com/example/demo/controller/GenreController.java
@@ -0,0 +1,188 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.exception.GenreNotFoundException;
+import com.example.demo.exception.MovieNotFoundException;
+import com.example.demo.model.Genre;
+import com.example.demo.model.MovieDetails;
+import com.example.demo.repository.GenreRepository;
+import com.example.demo.repository.MovieDetailsRepository;
+import com.example.demo.service.GenreService;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping(value = "/api/movies")
+@RestController
+public class GenreController {
+	
+	org.jboss.logging.Logger logger = LoggerFactory.logger(GenreController.class);
+	
+	@Autowired
+	private GenreRepository genreRepository;
+	@Autowired
+	private MovieDetailsRepository movieDetailsRepository;
+	@Autowired
+	GenreService genreService;
+	
+	
+//	Ok
+	@PostMapping(value = "/add_genre")
+	public String saveGenre(@RequestBody Genre genre) {
+		genreService.saveGenre(genre);
+		return "Genre Saved Successfully";
+	}
+	
+	
+	
+//	Get all genres
+//	ok
+	@GetMapping("/genres")
+	  public ResponseEntity<List<Genre>> getAllgenres() {
+	    List<Genre> Genre = new ArrayList<Genre>();
+	    genreRepository.findAll().forEach(Genre::add);
+	    if (Genre.isEmpty()) {
+	      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    }
+	    return new ResponseEntity<>(Genre, HttpStatus.OK);
+	  }
+	
+//	Get All Genres By Movie Id
+	@GetMapping("/{movieId}/genresById")
+	  public ResponseEntity<List<Genre>> getAllGenresByMovieId(@PathVariable(value = "movieId") int movieId) throws MovieNotFoundException {
+	    if (!movieDetailsRepository.existsById(movieId)) {
+	      throw new MovieNotFoundException("Not found Movie with id = " + movieId);
+	    }
+	    List<Genre> genres = genreRepository.findGenreById(movieId);
+	    if (genres.isEmpty()) {
+		      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		    }
+	    return new ResponseEntity<>(genres, HttpStatus.OK);
+	  }
+	
+
+//	USER
+//	Get All movies by genre_id
+//	Not Satisfied
+	  @GetMapping("/genres/{genreId}")
+//	  @PreAuthorize("hasRole('USER')")
+	  public ResponseEntity<Genre> getGenresById(@PathVariable(value = "genreId") int genreId) throws GenreNotFoundException {
+		  Genre genre = genreRepository.findById(genreId)
+	        .orElseThrow(() -> new GenreNotFoundException("Not found genre with id = " + genreId));
+	    return new ResponseEntity<>(genre, HttpStatus.OK);
+	  }
+	  
+//	  Get All movies By Genre Id
+//	  @GetMapping("/genres/{genreId}/movies")
+//	  public ResponseEntity<List<MovieDetails>> getAllMoviessByGenreId(@PathVariable(value = "genreId") int genreId) throws GenreNotFoundException {
+//	    if (!genreRepository.existsById(genreId)) {
+//	      throw new GenreNotFoundException("Not found Genre  with id = " + genreId);
+//	    }
+//	    List<MovieDetails> movies = movieDetailsRepository.findMovieBygenreId(genreId);
+//	    return new ResponseEntity<>(movies, HttpStatus.OK);
+//	  }
+	  
+
+//	  Ok
+//	  Add Genres to existing movies
+	  @PostMapping("/{movieId}/genres")
+	  public ResponseEntity<Genre> addGenre(@PathVariable(value = "movieId") int movieId, @RequestBody Genre genreRequest) throws MovieNotFoundException, GenreNotFoundException{
+		  Genre genre = movieDetailsRepository.findById(movieId).map(movie -> {
+			  Integer genreId = genreRequest.getId();
+		      // Genre is existed
+			  if (genreId != 0L) {
+			  	Genre _genre = null;
+				try {
+					_genre = genreRepository.findById(genreId)
+							  .orElseThrow(()-> new GenreNotFoundException("Not Found genre with Id: " + genreId));
+				} catch (GenreNotFoundException e) {
+					e.printStackTrace();
+				}
+				  movie.addGenre(_genre);
+				  movieDetailsRepository.save(movie);
+				  return _genre;
+			  }
+			  logger.info("**********genreRequest Genner Control(125)*********: " + genreRequest);
+		      // add and create new Genre
+			  movie.addGenre(genreRequest);
+			  return genreRepository.save(genreRequest);
+		  }).orElseThrow(() -> new MovieNotFoundException("not Found Movie with Id: " + movieId ));
+		    return new ResponseEntity<>(genre, HttpStatus.CREATED);
+	  }
+	  
+	  
+	  
+	  
+	  @PostMapping("/mapGenre/{movieId}")
+	  public void mapGenre(@PathVariable(value = "movieId") int movieId, @RequestBody List<Integer> genreRequest) throws MovieNotFoundException, GenreNotFoundException {
+		  MovieDetails movie = movieDetailsRepository.findById(movieId)
+				  .orElseThrow(()-> new GenreNotFoundException("Not Found genre with Id: " + movieId));
+		  genreRequest.forEach((Integer id)->{
+			  
+			  movie.addGenre(genreRepository.getById(id));
+		  });
+		  movieDetailsRepository.save(movie);
+	  }		 
+	  
+	  
+	  
+	  
+	  
+	  
+//	  Updating a Genre
+//	  Ok
+	  @PutMapping("/update/genres/{id}")
+	  public ResponseEntity<Genre> updateGenre(@PathVariable("id") Integer id, @RequestBody Genre genreRequest) throws GenreNotFoundException  {
+	    Genre genre = genreRepository.findById(id)
+	        .orElseThrow(() -> new GenreNotFoundException("genreId " + id + "not found"));
+	    genre.setMovie_genre(genreRequest.getMovie_genre());
+	    return new ResponseEntity<>(genreRepository.save(genre), HttpStatus.OK);
+
+//	    {
+//	        "movie_genre": "Thriller"
+//	    }
+	    
+	  }
+	 
+	  @DeleteMapping("/{movieId}/genres/{genreId}")
+	  public ResponseEntity<HttpStatus> deleteGenreFromMovie(@PathVariable(value = "movieId") Integer movieId, @PathVariable(value = "genreId") Long genreId) throws MovieNotFoundException {
+	    MovieDetails movie = movieDetailsRepository.findById(movieId)
+	        .orElseThrow(() -> new MovieNotFoundException("Not found Movie with id = " + movieId));
+	    
+	    movie.removeGenre(genreId);
+	    movieDetailsRepository.save(movie);
+	    
+	    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	  }
+	  
+	  @DeleteMapping("/delete/genres/{id}")
+	  public ResponseEntity<HttpStatus> deleteGenre(@PathVariable("id") Integer id) {
+	    genreRepository.deleteById(id);
+	    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	  }
+	  
+//	  @GetMapping("/get_movie_by_id/{genre_id}")
+//	  public List<MovieGenre> findBymovie_id(@PathVariable(value = "genre_id" ) Integer genre_id){
+//		  return movieGenreRepository.existsById(genre_id);
+//	  }
+	  
+}
---
+++ Ott_DemoApp/src/main/java/com/example/demo/model/MovieDetails.java
@@ -0,0 +1,218 @@
+package com.example.demo.model;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name = "movie_details", schema = "movie_details")
+public class MovieDetails {
+	
+	@Column(name = "movie_name")
+	@NotBlank
+	private String movie_name;
+	
+	@NotNull
+	@Id
+	@Column(name = "movie_id")
+	private Integer movieId;
+	
+	@Column(name = "movie_title")
+	@NotBlank
+	private String movie_title;
+	
+	@Column(name = "movie_description")
+	private String movie_description;
+	
+	@Column(name = "movie_budget")
+	private Long movie_budget;
+	
+	@Column(name = "m_release_date")
+	@Temporal(TemporalType.DATE)
+	private Date m_release_date;
+	
+	@Column(name = "m_revenue")
+	private Long m_revenue;
+	
+	@Column(name = "m_popularity")
+	private Float m_popularity;
+	
+	@Column(name = "poster_path")
+	@NotBlank
+	private String posterPath;
+	
+	@Column(name = "backdrop_path")
+	@NotBlank
+	private String backdropPath;
+	
+	@Column(name = "media_path")
+	@NotBlank
+	private String mediaPath;
+
+	@ManyToMany(fetch = FetchType.LAZY,
+		      cascade = {
+		          CascadeType.PERSIST,
+		          CascadeType.MERGE
+		      })
+    @JoinTable(name = "movie_genres", schema = "movie_details",
+		      joinColumns = { @JoinColumn(name = "movie_id") },
+		      inverseJoinColumns = { @JoinColumn(name = "genre_id") })
+	private Set<Genre> genres = new HashSet<>();
+	
+	public MovieDetails() {
+	}
+	
+	
+
+	public MovieDetails(@NotBlank String movie_name, @NotNull Integer movieId, @NotBlank String movie_title,
+			String movie_description, Long movie_budget, Date m_release_date, Long m_revenue,
+			Float m_popularity, @NotBlank String posterPath, @NotBlank String backdropPath,
+			@NotBlank String mediaPath, Set<Genre> genres) {
+		super();
+		this.movie_name = movie_name;
+		this.movieId = movieId;
+		this.movie_title = movie_title;
+		this.movie_description = movie_description;
+		this.movie_budget = movie_budget;
+		this.m_release_date = m_release_date;
+		this.m_revenue = m_revenue;
+		this.m_popularity = m_popularity;
+		this.posterPath = posterPath;
+		this.backdropPath = backdropPath;
+		this.mediaPath = mediaPath;
+		this.genres = genres;
+	}
+
+
+
+	public String getMovie_name() {
+		return movie_name;
+	}
+
+	public void setMovie_name(String movie_name) {
+		this.movie_name = movie_name;
+	}
+
+	public Integer getMovie_id() {
+		return movieId;
+	}
+
+	public void setMovie_id(Integer movie_id) {
+		this.movieId = movie_id;
+	}
+
+	public String getMovie_title() {
+		return movie_title;
+	}
+
+	public void setMovie_title(String movie_title) {
+		this.movie_title = movie_title;
+	}
+
+	public String getMovie_description() {
+		return movie_description;
+	}
+
+	public void setMovie_description(String movie_description) {
+		this.movie_description = movie_description;
+	}
+
+	public Long getMovie_budget() {
+		return movie_budget;
+	}
+
+	public void setMovie_budget(Long movie_budget) {
+		this.movie_budget = movie_budget;
+	}
+
+	public Date getM_release_date() {
+		return m_release_date;
+	}
+
+	public void setM_release_date(Date m_release_date) {
+		this.m_release_date = m_release_date;
+	}
+
+	public Long getM_revenue() {
+		return m_revenue;
+	}
+
+	public void setM_revenue(Long m_revenue) {
+		this.m_revenue = m_revenue;
+	}
+
+	public Float getM_popularity() {
+		return m_popularity;
+	}
+
+	public void setM_popularity(Float m_popularity) {
+		this.m_popularity = m_popularity;
+	}
+
+	
+	  
+	public Integer getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Integer movieId) {
+		this.movieId = movieId;
+	}
+
+
+
+//	public List<Genre> getGenres() {
+//		return genres;
+//	}
+//
+//	public void setGenres(List<Genre> genres) {
+//		this.genres = genres;
+//	}
+
+	public void addGenre(Genre _genre) {
+	    this.genres.add(_genre);
+	    _genre.getMovies().add(this);
+	}
+	
+	public void removeGenre(long genreId) {
+	    Genre genre = this.genres.stream().filter(t -> t.getId() == genreId).findFirst().orElse(null);
+	    if (genre != null) {
+	      this.genres.remove(genre);
+	      genre.getMovies().remove(this);
+	    }
+	}
+
+	public String getPosterPath() {
+		return posterPath;
+	}
+
+	public void setPosterPath(String posterPath) {
+		this.posterPath = posterPath;
+	}
+
+	public String getBackdropPath() {
+		return backdropPath;
+	}
+
+	public void setBackdropPath(String backdropPath) {
+		this.backdropPath = backdropPath;
+	}
+
+	public String getMediaPath() {
+		return mediaPath;
+	}
+
+	public void setMediaPath(String mediaPath) {
+		this.mediaPath = mediaPath;
+	}
+	
+}
